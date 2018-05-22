aa4fea361e2ba9fa227eaf2a8e944dc76fcd4e78
Miguel <@empimcf@gmail.com>
2018-05-22 16:12:18 +0200

    cambios colorrrr

--- app/src/main/java/com/example/miguel/lolstats/RiotApiHelper.java
+++ app/src/main/java/com/example/miguel/lolstats/RiotApiHelper.java
@@ -17,7 +17,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Map;
 
 public class RiotApiHelper {
